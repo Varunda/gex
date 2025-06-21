6faa8171edbf33ab928efdebf1dcc34db7ea4fb0
Varunda <trvarunda@gmail.com>
2025-06-21 10:11:32 -0700

    fix missing Vue filter from luxon migration

--- gex/src/filters/CompactTimeAgoFilter.ts
+++ gex/src/filters/CompactTimeAgoFilter.ts
@@ -1,5 +1,35 @@
 import Vue from "vue";
+import * as luxon from "luxon";
 
 Vue.filter("compactTimeAgo", (date: Date | number): string => {
-    throw `not implemented in Luxon migration yet`;
+	if (typeof date == "number") {
+		date = new Date(date);
+    }
+
+    const m = luxon.DateTime.fromJSDate(date);
+    const now = luxon.DateTime.fromJSDate(new Date(Date.now()));
+
+	const years = now.diff(m, "years").as("years");
+	const months = now.diff(m, "months").as("months") % 12;
+
+	if (years >= 1) {
+		return `${years}Y ${months}M`;
+	}
+
+	const days = Math.floor(now.diff(m, "days").as("days"));
+	if (months >= 1) {
+		return `${months}M ${days % 30}d`;
+	}
+
+	const hours = Math.floor(now.diff(m, "hours").as("hours")) % 24;
+	if (days >= 1) {
+		return `${days}d ${hours}h`;
+	}
+
+	const mins = Math.floor(now.diff(m, "minutes").as("minutes")) % 60;
+	if (hours >= 1) {
+        return `${hours} hour${hours == 1 ? "" : "s"}`;
+    }
+
+	return `${mins} minutes`;
 });
